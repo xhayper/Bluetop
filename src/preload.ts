38ed05045609c48f5f7524934807305b6b09e492
xhayper <hayper1919@gmail.com>
2023-09-13 21:28:44 +0700

    feat: update the scrollbar abit

--- src/preload.ts
+++ src/preload.ts
@@ -11,20 +11,20 @@ document.addEventListener('DOMContentLoaded', () => {
   addStyle(`
 ::-webkit-scrollbar-track
 {
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
     border-radius: 10px;
     background-color: rgba(0, 0, 0, 0.1);
 }
   
 ::-webkit-scrollbar {
-    width: 8px;
+    width: 6px;
     background-color: #F5F5F5;
 }
   
 ::-webkit-scrollbar-thumb {
     border-radius: 10px;
     -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
-    background-color: #3F3F3F;
+    background-color: #555;
 }
   `);
 });
