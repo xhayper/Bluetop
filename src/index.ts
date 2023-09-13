9e015135a0bfad61dc388db0284943dcfa93e491
xhayper <hayper1919@gmail.com>
2023-09-13 21:09:02 +0700

    feat: update the project

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { app, shell, BrowserWindow } from 'electron';
+import Store from 'electron-store';
+import path from 'node:path';
+
+if (!app.requestSingleInstanceLock()) app.quit();
+
+const configStore = new Store<{
+  winBounds?: { width?: number; height?: number; x?: number; y?: number };
+}>();
+
+const createWindow = () => {
+  const { width, height, x, y } = configStore.get('winBounds') ?? {};
+
+  const mainWindow = new BrowserWindow({
+    width: width ?? 1280,
+    height: height ?? 720,
+    x,
+    y,
+
+    title: 'Bluetop',
+    icon: path.join(__dirname, '../', 'build', 'icon.ico'),
+    backgroundColor: '#000000',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.setMenu(null);
+
+  mainWindow.on('close', () => {
+    configStore.set('winBounds', mainWindow.getBounds());
+  });
+
+  mainWindow.on('page-title-updated', (event: any) => {
+    event.preventDefault();
+  });
+
+  mainWindow.webContents.on('before-input-event', (_, input) => {
+    if (input.type === 'keyDown' && input.key === 'F12') mainWindow.webContents.toggleDevTools();
+  });
+
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+
+  mainWindow.loadURL(path.join(__dirname, '../', 'index.html'));
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ src/preload.ts
@@ -0,0 +1,30 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+const addStyle = (styleString: string) => {
+  const style = document.createElement('style');
+  style.textContent = styleString;
+  document.head.append(style);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  addStyle(`
+::-webkit-scrollbar-track
+{
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    border-radius: 10px;
+    background-color: rgba(0, 0, 0, 0.1);
+}
+  
+::-webkit-scrollbar {
+    width: 8px;
+    background-color: #F5F5F5;
+}
+  
+::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
+    background-color: #3F3F3F;
+}
+  `);
+});
